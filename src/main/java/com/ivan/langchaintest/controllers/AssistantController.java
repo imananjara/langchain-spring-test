d655c50c1ded3b3084b2190c431fa544ffc72b83
imananjara <ivan.mananjara@gmail.com>
2024-10-29 23:23:53 +0100

    Resumer plusieurs phrases

--- src/main/java/com/ivan/langchaintest/controllers/AssistantController.java
+++ src/main/java/com/ivan/langchaintest/controllers/AssistantController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/assistant")
 public class AssistantController {
@@ -16,8 +20,16 @@ public class AssistantController {
         this.chatLanguageModel = chatLanguageModel;
     }
 
-    @GetMapping("/categorize")
+    @GetMapping("/summaryTexts")
     public String summaryMultipleTextsIntoOne() {
-        return chatLanguageModel.generate("Hello Gemini!");
+        List<String> texts = new ArrayList<>();
+        texts.add("J'ai un problème de communication avec mon directeur.");
+        texts.add("Je n'arrive pas à négocier avec les clients le prix du nouvel ordinateur.");
+        texts.add("Le prix des composants de l'ordinateur a explosé.");
+        texts.add("Le camion qui devait transporter les composants est tombé en panne.");
+
+        String input = "Est-ce que tu peux, à partir de ces phrases, me dire, en 1 phrase, pourquoi le projet a échoué.: " + String.join(" ", texts);
+
+        return chatLanguageModel.generate(input);
     }
 }
